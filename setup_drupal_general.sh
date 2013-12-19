68c988d8057c0e541c520a602fa3464fa0620200
Florian Konnertz <fkonnertz@lavabit.com>
2013-12-19 15:04:38 +0100

    add initial versions of setup scripts

---
+++ setup_drupal_general.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+drush vset admin_menu_margin_top 1
+drush vset admin_menu_position_fixed 1
+drush vset admin_menu_tweak_modules 1
+drush vset admin_menu_tweak_permissions 1
+drush vset admin_menu_tweak_tabs 0
+
---
+++ setup_site.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+distro_name="politeia"
+site_base="polis"
+site_tld="utopiaspora.net"
+site_prod="polis.utopiaspora.net"
+
+usage() {
+  echo "USAGE: `basename $0` <site:dev|staging|prod>"
+  exit
+}
+
+# search string is the first argument and the rest are the array elements
+contains() {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+
+avail=("dev" "staging" "prod")
+if contains "$1" "${avail[@]}"
+then 
+  echo "$1 is defined."
+else
+  echo "ERROR: $1 is not defined as site"
+  usage
+fi
+
+if test "$1" == "prod"
+then
+  SITE=$site_prod
+  DB="prod"
+else
+  SITE="$site_base-$1.$site_tld"
+  DB="$1"
+fi
+
+echo "Setting up $SITE \n"
+
+
+DRUPAL_ROOT=/var/www/vhosts/$SITE/s
+INSTALL_DIR=$DRUPAL_ROOT/sites/default
+export DRUPAL_ROOT
+export INSTALL_DIR
+
+echo "change to directory $DRUPAL_ROOT"
+cd $DRUPAL_ROOT
+
+
+echo "delete all below $DRUPAL_ROOT"
+echo "superuser password needed!"
+sudo rm * -r
+
+echo "\nsetup drupal site according to drush makefile..."
+drush make "https://github.com/groovehunter/$site_distro.git;a=blob_plain;f=build-$site_distro.make;hb=HEAD" -y
+
+if test "$?" != 0
+then
+  echo "drush make FAILED. Exiting..."
+  exit
+fi
+echo $?
+#exit
+
+echo "\ninstall drupal instance..."
+#drush si $distro_name --db-url="mysql://drupal:$passwd@localhost/drupal_$site_name_$DB" -y
+drush si $distro_name --db-url="mysql://drupal:$passwd@localhost/drupal_$site_name_$DB" -y
+# OK?
+
+echo "change permissions in install folder"
+sudo chgrp www-data $INSTALL_DIR/files -R
+sudo chmod g+w $INSTALL_DIR/files -R
+
+### general drupal tweaks
+setup_drupal_general.sh
+
+echo "change directory to custom module folder"
+cd $DRUPAL_ROOT/profiles/$distro_name/modules
+mkdir $site_name
+cd $site_name
+
+
+echo "get custom modules via git clone..."
+echo "NONE"
+
+echo "change directory to install folder $INSTALL_DIR"
+cd $INSTALL_DIR
+
+
+### user, roles, ldap
+echo "drush create roles..."
+drush scr $DRUPAL_ROOT/profiles/$distro_name/modules/distro_name/config/create_roles.script drupal_roles_dev create
+
+cd $DRUPAL_ROOT/profiles/$site_distro/modules/contrib
+# clone l10n_update dev version
+# git clone --recursive --branch 7.x-1.x http://git.drupal.org/project/l10n_update.git
+# clone taxonomy_csv with fixed issue https://drupal.org/node/1475952
+echo "change directory to install folder $INSTALL_DIR"
+cd $INSTALL_DIR
+
+
+
+### sonstiges
+echo "setting site variables..."
+drush vset site_name "$site_name"
+drush vset site_default_country de
+drush vset proxy_server "auproxy.syscomp.de"
+drush vset proxy_port "3128"
+drush vset configurable_timezones 0
+drush vset date_default_timezone "Europe/Berlin"
+drush vset user_default_timezone: "0"
+drush vset date_first_day "1"
+
+
+# other vars
+echo "setting further variables..."
+drush vset date_format_short "d.m.Y"
+
+drush en l10n_update -y
+drush language-add de
+drush language-default de
+drush l10n-update
+
+
+echo "FINISHED setup script. Check above for errors!"
+
+
+
